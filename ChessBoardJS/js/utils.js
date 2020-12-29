e9789d472a09cd21a03b13144606aa0ee890d5b6
SamDg <sam.dg019@gmail.com>
2020-12-29 19:57:30 +0800

    JavaScript ChessBoard

---
+++ ChessBoardJS/js/utils.js
@@ -0,0 +1,9 @@
+// returns bool
+export const getClassListIncludes = function (ElementId, className) {
+    return document.getElementById(ElementId).classList.value.includes(className);
+};
+
+// set innerHTML
+export const setInnerHtml = function (ElementId, InputString) {
+    document.getElementById(ElementId).children[0].innerHTML = InputString;
+};
