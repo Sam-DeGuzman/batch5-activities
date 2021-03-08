1afc08513ba581d1199d1dc9990d7ad98e2558a8
SamDg <sam.dg019@gmail.com>
2021-03-08 20:46:55 +0800

    First Activity SQL

---
+++ SQL/first_activity.sql
@@ -0,0 +1,33 @@
+/* Creating Table */
+
+CREATE TABLE students(
+	id INT, first_name VARCHAR(255),middle_name VARCHAR(255),last_name VARCHAR(255),age INT, location VARCHAR(255)
+);
+
+
+
+/* Inserting Data */
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location) 
+VALUES(1,"Juan","Blank","Cruz",18,"Manila"),
+(2,"John","Blank","Young",20,"Manila"),
+(3,"Victor","Blank","Rivera",21,"Manila"),
+(4,"Adrian","Blank","Co",19,"Laguna"),
+(5,"Pau","Blank","Riosa",22,"Marikina"),
+(6,"Piolo","Blank","Pascual",25,"Manila")
+
+
+
+/* Updating first record */
+
+UPDATE students
+SET first_name = "Ana", middle_name="Cui", last_name="Cajocson", age=25, location="Bulacan"
+WHERE id = 1;
+
+
+
+
+
+
+
+
